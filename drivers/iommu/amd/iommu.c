13c3f5bd3eb2763af3fd7473fe456d8fb746c5a0
Alejandro Jimenez <alejandro.j.jimenez@oracle.com>
2024-12-13 16:41:56 +0000

    iommu/amd: Preserve default DTE fields when updating Host Page Table Root
    When updating the page table root field on the DTE, avoid overwriting any bits
    that are already set. The earlier call to make_clear_dte() writes default values
    that all DTEs must have set (currently DTE[V]), and those must be preserved.
    
    Currently this doesn't cause problems since the page table root udpdate is the
    first field that is set after make_clear_dte() is called, and DTE_FLAG_V is set
    again later along with the permission bits (IR/IW). Remove this redundant
    assignment too.
    
    Signed-off-by: Alejandro Jimenez <alejandro.j.jimenez@oracle.com>

--- drivers/iommu/amd/iommu.c
+++ drivers/iommu/amd/iommu.c
@@ -2043,7 +2043,7 @@ static void set_dte_entry(struct amd_iommu *iommu,
 	make_clear_dte(dev_data, dte, &new);
 
 	if (domain->iop.mode != PAGE_MODE_NONE)
-		new.data[0] = iommu_virt_to_phys(domain->iop.root);
+		new.data[0] |= iommu_virt_to_phys(domain->iop.root);
 
 	/*
 	 * DTE[Mode] must be set to 0 when using v2 page table i.e. nested
@@ -2057,7 +2057,7 @@ static void set_dte_entry(struct amd_iommu *iommu,
 	new.data[0] |= (domain->iop.mode & DEV_ENTRY_MODE_MASK)
 		    << DEV_ENTRY_MODE_SHIFT;
 
-	new.data[0] |= DTE_FLAG_IR | DTE_FLAG_IW | DTE_FLAG_V;
+	new.data[0] |= DTE_FLAG_IR | DTE_FLAG_IW;
 
 	/*
 	 * When SNP is enabled, we can only support TV=1 with non-zero domain ID.
